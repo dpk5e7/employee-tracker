9ad9f55ec75b8f69a98fd2a60096f55fe0a55520
Dan Kelly <dpk5e7@yahoo.com>
2022-08-30 23:04:51 -0600

    dotenv

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
+
 const EmployeeTracker = require("./lib/EmployeeTracker");
 
-// Initialize a new TeamProfileGenerator object
 const et = new EmployeeTracker();
 
-// Start generating
 et.start();
