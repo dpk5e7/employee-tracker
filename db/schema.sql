f475d64134b6a858b733f62b5a1b336d1345f94b
Dan Kelly <dpk5e7@yahoo.com>
2022-08-30 20:33:02 -0600

    Framework and database scripts

--- db/schema.sql
+++ db/schema.sql
@@ -1,19 +1,37 @@
 -- Write your Schema Here -- 
-DROP DATABASE IF EXISTS movies_db;
-CREATE DATABASE movies_db;
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
 
-USE movies_db;
+USE employee_tracker_db;
 
-CREATE TABLE movies (
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  movie_name VARCHAR(30) NOT NULL
+  name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE reviews (
+CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  movie_id INT,
-  review TEXT,
-  FOREIGN KEY (movie_id)
-  REFERENCES movies(id)
-  ON DELETE SET NULL
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT,
+  CONSTRAINT fk_department_id
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  CONSTRAINT fk_role_id
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+  CONSTRAINT fk_manager_id
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,24 @@
-INSERT INTO movies (movie_name)
-VALUES ("A New Hope"),
-       ("The Empire Strikes Back"),
-       ("Return of the Jedi"),
-       ("The Phantom Menace"),
-       ("Attack of the Clones"),
-       ("Revenge of the Sith");
+INSERT INTO department (name)
+VALUES ("Force Users"),
+       ("Clone Army"),
+       ("Gangsters");
 
-INSERT INTO reviews (movie_id, review)
-VALUES (1, "1977"),
-       (2, "1980"),
-       (3, "1983"),
-       (4, "1999"),
-       (5, "2002"),
-       (6, "2005");
+INSERT INTO role (title, salary, department_id)
+VALUES ("Jedi", 100, 1),
+       ("Sith", 99, 1),
+       ("Commander", 91, 2),
+       ("Captain", 90, 2),
+       ("Bounty Hunters", 81, 3),
+       ("Smugglers", 80, 3);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Ben", "Kenobi", 1, null),
+       ("Luke", "Skywalker", 1, 1),
+       ("Darth", "Sidious", 2, null),
+       ("Darth", "Vader", 2, 3),
+       ("Cody", "Clone", 3, null),
+       ("Rex", "Clone", 4, 5),
+       ("Boba", "Fett", 5, null),
+       ("Han", "Solo", 6, null),
+       ("Chew", "bacca", 6, 8);
        
